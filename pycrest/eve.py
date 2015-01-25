ba04da319a314a77cc87628e73363a7b22291a47
Sairon Istyar <saironiq@gmail.com>
2015-01-25 04:02:32 +0100

    add optional disk caching; replace hashdict with frozenset builtin

--- pycrest/eve.py
+++ pycrest/eve.py
@@ -1,11 +1,13 @@
+import os
 import base64
 import requests
 import time
 import urlparse
+import zlib
+import cPickle
 from pycrest import version
 from pycrest.compat import bytes_, text_
 from pycrest.errors import APIException
-from pycrest.hashdict import hashdict
 
 try:
     from urllib.parse import quote
@@ -16,12 +18,56 @@ import logging
 logger = logging.getLogger("pycrest.eve")
 
 
+class APICache(object):
+    def __init__(self, path):
+        self._cache = {}
+        self.path = path
+        if not os.path.isdir(self.path):
+            os.mkdir(self.path, mode=0700)
+
+    def _getpath(self, key):
+        return os.path.join(self.path, str(hash(key)), '.cache')
+
+    def put(self, key, value):
+        with open(self._getpath(key), 'w') as f:
+            f.write(zlib.compress(cPickle.dumps(value, -1)))
+        self._cache[key] = value
+
+    def get(self, key):
+        if key in self._cache:
+            return self._cache[key]
+
+        try:
+            with open(self._getpath(key), 'r') as f:
+                return cPickle.loads(zlib.decompress(f.read()))
+        except IOError, ex:
+            if ex.errno == 2:  # file does not exist (yet)
+                return None
+            else:
+                raise ex
+
+    def invalidate(self, key):
+        self._cache.pop(key, None)
+
+        try:
+            os.unlink(self._getpath(key))
+        except OSError, ex:
+            if ex.errno == 2:  # does not exist
+                pass
+            else:
+                raise ex
+
+
 class APIConnection(object):
-    def __init__(self, additional_headers=None, user_agent=None, cache_time=600):
+    def __init__(self, additional_headers=None, user_agent=None, cache_time=300, cache_dir=None):
         self._headers = {} if not additional_headers else additional_headers
         self._useragent = "PyCrest v %s" % version if not user_agent else user_agent
         self.cache_time = cache_time
         self.session = requests.Session()
+        if cache_dir:
+            self.cache = APICache(cache_dir)
+        else:
+            self.cache = None
 
     def get(self, resource, params={}):
         headers = {
@@ -38,11 +84,28 @@ class APIConnection(object):
         for key in params:
             prms[key] = params[key]
 
+        # check cache
+        if self.cache:
+            key = (resource, frozenset(headers.items()), frozenset(prms.items()))
+            cached = self.cache.get(key)
+            if cached and time.time() - cached['timestamp'] < self.cache_time:
+                return cached['payload']
+            elif cached:
+                self.cache.invalidate(key)
+
         logger.debug('Getting resource %s (params=%s)', resource, prms)
         res = self.session.get(resource, headers=headers, params=prms)
         if res.status_code != 200:
             raise APIException("Got unexpected status code from server: %i" % res.status_code)
-        return res.json()
+
+        ret = res.json()
+
+        # cache result
+        if self.cache:
+            key = (resource, frozenset(headers.items()), frozenset(prms.items()))
+            self.cache.put(key, {'timestamp': time.time(), 'payload': ret})
+
+        return ret
 
 
 class EVE(APIConnection):
@@ -74,7 +137,6 @@ class EVE(APIConnection):
 
     def __getattr__(self, item):
         return self._data.__getattr__(item)
-
     def auth_uri(self, scopes=None, state=None):
         s = [] if not scopes else scopes
         return "%s/authorize?response_type=code&redirect_uri=%s&client_id=%s%s%s" % (
@@ -168,7 +230,7 @@ class APIObject(object):
         return self._dict[item]
 
     def __call__(self, **kwargs):
-        hkwargs = hashdict(kwargs)
+        hkwargs = frozenset(kwargs.items())
         cached = hkwargs in self._cache
         if ((not cached) or int(time.time()) - self._cache[hkwargs][0] > self.connection.cache_time) and 'href' in self._dict:
             logger.debug("%s with params %s not yet loaded", self._dict['href'], kwargs)
