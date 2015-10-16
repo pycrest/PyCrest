58580e1941e365424a802c60de1e821bc7f952f1
blitzmann <ryan.xgamer99@gmail.com>
2015-10-16 16:34:16 -0400

    Fix #14 - whoami() doesn't work for test server

--- pycrest/eve.py
+++ pycrest/eve.py
@@ -210,7 +210,7 @@ class EVE(APIConnection):
             self._oauth_endpoint,
             quote(self.redirect_uri, safe=''),
             self.client_id,
-            "&scope=%s" % ','.join(s) if scopes else '',
+            "&scope=%s" % ' '.join(s) if scopes else '',
             "&state=%s" % state if state else ''
         )
 
@@ -272,7 +272,7 @@ class AuthedConnection(EVE):
 
     def whoami(self):
         if 'whoami' not in self._cache:
-            self._cache['whoami'] = self.get("https://login.eveonline.com/oauth/verify")
+            self._cache['whoami'] = self.get("%s/verify" % self._oauth_endpoint)
         return self._cache['whoami']
 
     def refresh(self):
